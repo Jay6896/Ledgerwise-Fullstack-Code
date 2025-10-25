a265c9779cb28df5b6a7c620236b8882e124ddee
Jay6896 <jamilsalami08@gmail.com>
2025-10-25 06:09:14 +0100

    Web Host Config

--- backend/app.py
+++ backend/app.py
@@ -17,6 +17,7 @@ def create_app():
     CORS(app, supports_credentials=True, resources={
         r"/*": {
             "origins": [
+                "https://ledgerwise-chi.vercel.app/",
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:8080",
